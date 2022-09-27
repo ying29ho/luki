5b88fdc77ea3240f03445e16f69a0425eff24827
ying29 <ying29ho@gmail.com>
2022-09-27 20:01:43 +0800

    test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import Layout from "./Layout";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from "./reportWebVitals";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
